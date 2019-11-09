4581b6c52e7cb3b2a994fc0dbf48d8f288ecf697
League Student <league@iMac-15.attlocal.net>
2019-11-08 18:05:32 -0800

    :smile:

--- src/_06_gridworld/GridWorld.java
+++ src/_06_gridworld/GridWorld.java
@@ -1,22 +1,48 @@
 package _06_gridworld;
 
+import java.awt.Color;
 import java.util.Random;
 
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public  class GridWorld {
 	public static void main(String[] args) {
 		World world = new World();
+		
 		world.show();
 		Bug bug = new Bug();
-		Random random = new Random(200);
+		Random gen = new Random();
+		Bug bug2 = new Bug();
+		bug2.setColor(Color.BLUE);
 		
-		Location location = new Location(random, random);
+		Flower flower = new Flower();
+		Flower flower1 = new Flower();
+		int x = gen.nextInt(9);
+		int y = gen.nextInt(9);
+		Location location3 = new Location(x, y+1);
+		
+		Location location4 = new Location(x, y-1);
+	Location location2 = new Location(x, y);
+		Location location = new Location(1, 1); 
+		
+		world.add(location, bug);
+		world.add(location2, bug2);
+		world.add(location3, flower);
+		world.add(location4, flower1);
+		
+		bug.moveTo(location);
+		bug2.moveTo(location2);
+		
 		bug.setDirection(90);
 		
-		bug.setColor(BLUE);
+		bug.setColor(null);
+		
+		
+		
+		
 	}
 	
 
